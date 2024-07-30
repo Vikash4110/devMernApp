4bd8b13c3f0f4f28d25fc25e362816a4f5ba23c1
Vikash4110 <139474262+Vikash4110@users.noreply.github.com>
2024-07-31 05:07:06 +0530

    goodluck

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const Port = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://chaintech-network-nine.vercel.app/',
     methods: 'GET, POST, PUT, DELETE, PATCH, HEAD',
     credentials: true,
 };
